e84becbce15056f0bf25802273b82953c6deeddb
Collin Styles <collingstyles@gmail.com>
2020-12-04 15:29:55 -0800

    02: Performance improvements
    Global:
    - Use a `&str` instead of allocating a string
    
    Part 1:
    - Don't use a `HashMap` to count all characters; just count the one we
      care about
    
    Part 2:
    - Only iterate over the characters once by grabbing `char2` from the
      same iterator used to grab `char1`

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,4 @@
 use regex::Regex;
-use std::collections::HashMap;
 
 static INPUT: &str = include_str!("../input.txt");
 
@@ -10,47 +9,43 @@ lazy_static::lazy_static! (
 );
 
 #[derive(Default, Debug, Clone)]
-struct Password {
+struct Password<'a> {
     required_letter: char,
-    min: i32,
-    max: i32,
-    string: String,
+    min: usize,
+    max: usize,
+    string: &'a str,
 }
 
-impl Password {
+impl<'a> Password<'a> {
     fn is_valid_part_1(&self) -> bool {
-        let mut hm: HashMap<char, i32> = HashMap::with_capacity(self.string.len());
-        for character in self.string.chars() {
-            hm.entry(character).and_modify(|i| *i += 1).or_insert(1);
-        }
+        let count = self
+            .string
+            .chars()
+            .filter(|c| *c == self.required_letter)
+            .count();
 
-        match hm.get(&self.required_letter) {
-            None => false,
-            Some(count) if *count >= self.min && *count <= self.max => true,
-            _ => false,
-        }
+        count >= self.min && count <= self.max
     }
 
     fn is_valid_part_2(&self) -> bool {
-        let index1 = (self.min - 1) as usize;
-        let char1 = self.string.chars().nth(index1).unwrap();
+        let mut chars = self.string.chars();
 
-        let index2 = (self.max - 1) as usize;
-        let char2 = self.string.chars().nth(index2).unwrap();
+        let char1 = chars.nth(self.min - 1).unwrap();
+        let char2 = chars.nth(self.max - self.min - 1).unwrap();
 
         (char1 == self.required_letter) ^ (char2 == self.required_letter)
     }
 }
 
-impl std::convert::From<&str> for Password {
-    fn from(string: &str) -> Self {
+impl<'a> std::convert::From<&'a str> for Password<'a> {
+    fn from(string: &'a str) -> Self {
         let captures = RE.captures(string).expect("coudn't match regex");
 
         Self {
             required_letter: captures.get(3).unwrap().as_str().chars().next().unwrap(),
             min: captures.get(1).unwrap().as_str().parse().unwrap(),
             max: captures.get(2).unwrap().as_str().parse().unwrap(),
-            string: captures.get(4).unwrap().as_str().to_string(),
+            string: captures.get(4).unwrap().as_str(),
         }
     }
 }
@@ -66,13 +61,13 @@ fn main() {
     part2(&passwords);
 }
 
-fn part1(passwords: &[Password]) {
+fn part1<'a>(passwords: &'a [Password]) {
     let count: usize = passwords.iter().filter(|p| p.is_valid_part_1()).count();
 
     println!("part1 = {}", count);
 }
 
-fn part2(passwords: &[Password]) {
+fn part2<'a>(passwords: &'a [Password]) {
     let count: usize = passwords.iter().filter(|p| p.is_valid_part_2()).count();
 
     println!("part2 = {}", count);
