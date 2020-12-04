00c905d4f31fe0c9ade613f1f78e60a606c1cc94
Collin Styles <collingstyles@gmail.com>
2020-12-04 00:22:53 -0800

    04: Part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::convert::{TryFrom, TryInto};
 
 #[derive(Debug, Default)]
 struct Passport<'a> {
@@ -16,15 +17,75 @@ lazy_static::lazy_static!(
     static ref FIELD_REGEX: Regex = {
         Regex::new(r#"([^:]+):(.*)"#).unwrap()
     };
+
+    static ref HGT_REGEX: Regex = {
+        Regex::new(r#"^(\d+)(in|cm)$"#).unwrap()
+    };
+
+    static ref HCL_REGEX: Regex = {
+        Regex::new(r#"^#[0-9a-f]{6}$"#).unwrap()
+    };
+
+    static ref ECL_REGEX: Regex = {
+        Regex::new(r#"^(amb|blu|brn|gry|grn|hzl|oth)$"#).unwrap()
+    };
+
+    static ref PID_REGEX: Regex = {
+        Regex::new(r#"^\d{9}$"#).unwrap()
+    };
 );
 
 impl<'a> Passport<'a> {
-    fn is_valid(&self) -> bool {
+    fn is_valid_part_1(&self) -> bool {
         let necessary_fields = [
             self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid,
         ];
 
         necessary_fields.iter().all(|field| field.is_some())
+    }
+
+    fn is_valid_part_2(&self) -> Option<bool> {
+        let necessary_fields = vec![
+            self.byr, self.iyr, self.eyr, self.hgt, self.hcl, self.ecl, self.pid,
+        ];
+
+        let wow = necessary_fields
+            .into_iter()
+            .collect::<Option<Vec<&str>>>()?;
+
+        let byr: i32 = wow[0].parse().ok()?;
+        if byr < 1920 || byr > 2002 {
+            return Some(false);
+        }
+
+        let iyr: i32 = wow[1].parse().ok()?;
+        if iyr < 2010 || iyr > 2020 {
+            return Some(false);
+        }
+
+        let eyr: i32 = wow[2].parse().ok()?;
+        if eyr < 2020 || eyr > 2030 {
+            return Some(false);
+        }
+
+        let hgt: Height = wow[3].try_into().ok()?;
+        if !hgt.is_valid() {
+            return Some(false);
+        }
+
+        if !HCL_REGEX.is_match(wow[4]) {
+            return Some(false);
+        }
+
+        if !ECL_REGEX.is_match(wow[5]) {
+            return Some(false);
+        }
+
+        if !PID_REGEX.is_match(wow[6]) {
+            return Some(false);
+        }
+
+        Some(true)
     }
 }
 
@@ -57,18 +118,91 @@ impl<'a> std::convert::From<Vec<&'a str>> for Passport<'a> {
     }
 }
 
+enum Unit {
+    Inch,
+    Centimeter,
+}
+
+impl TryFrom<&str> for Unit {
+    type Error = ();
+
+    fn try_from(string: &str) -> std::result::Result<Self, Self::Error> {
+        match string {
+            "in" => Ok(Self::Inch),
+            "cm" => Ok(Self::Centimeter),
+            _ => Err(()),
+        }
+    }
+}
+
+struct Height {
+    unit: Unit,
+    value: i64,
+}
+
+impl TryFrom<&str> for Height {
+    type Error = ();
+
+    fn try_from(string: &str) -> std::result::Result<Self, Self::Error> {
+        let captures = HGT_REGEX.captures(string).ok_or(())?;
+
+        let value: i64 = captures
+            .get(1)
+            .expect("no value")
+            .as_str()
+            .parse()
+            .map_err(|_| ())?;
+
+        let unit: Unit = captures.get(2).expect("no unit").as_str().try_into()?;
+
+        Ok(Height { unit, value })
+    }
+}
+
+impl Height {
+    fn is_valid(&self) -> bool {
+        match self.unit {
+            Unit::Inch => self.value >= 59 && self.value <= 76,
+            Unit::Centimeter => self.value >= 150 && self.value <= 193,
+        }
+    }
+}
+
 fn main() {
     let split_regex = Regex::new(r#"\s"#).unwrap();
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let valid_passports: usize = input
+    let passports: Vec<Passport> = input
         .trim()
         .split("\n\n")
         .map(|group| split_regex.split(group))
         .map(|split| split.collect::<Vec<&str>>())
         .map(|fields| Passport::from(fields))
-        .filter(|passport| passport.is_valid())
+        .collect();
+
+    part1(&passports);
+    println!();
+    part2(&passports);
+}
+
+fn part1(passports: &[Passport]) {
+    println!("== PART 1 ==");
+
+    let valid_passports = passports
+        .iter()
+        .filter(|passport| passport.is_valid_part_1())
         .count();
 
-    println!("{:?}", valid_passports);
+    println!("{}", valid_passports);
+}
+
+fn part2(passports: &[Passport]) {
+    println!("== PART 2 ==");
+
+    let valid_passports = passports
+        .iter()
+        .filter(|passport| passport.is_valid_part_2().unwrap_or(false))
+        .count();
+
+    println!("{}", valid_passports);
 }
