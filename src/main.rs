6b3820fde9811fc92eb1d681cf8f2b7593eb6ca1
Collin Styles <collingstyles@gmail.com>
2020-12-03 18:37:02 -0800

    01: Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let numbers: Vec<i32> = input
+        .trim()
+        .split("\n")
+        .flat_map(|line| line.parse())
+        .collect();
+
+    let size = numbers.len();
+
+    for i in 0..size {
+        for j in 0..size {
+            let sum = numbers[i] + numbers[j];
+            if sum == 2020 {
+                let product = numbers[i] * numbers[j];
+                println!("i: {}, j: {}, product: {}", i, j, product);
+            }
+        }
+    }
 }
