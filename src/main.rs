8d494c705d82028d1a011fb0cf603eec7b28eb71
Collin Styles <collingstyles@gmail.com>
2020-12-03 18:50:43 -0800

    01: Break out into part 1 and part 2

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,32 @@ fn main() {
 
     let size = numbers.len();
 
+    part1(&numbers, size);
+    part2(&numbers, size);
+}
+
+fn part1(numbers: &[i32], size: usize) {
     for i in 0..size {
         for j in 0..size {
-            for k in 0..size {
-                let sum = numbers[i] + numbers[j] + numbers[k];
+            let sum = numbers[i] + numbers[j];
+            if sum == 2020 {
+                let product = numbers[i] * numbers[j];
+                println!("i: {}, j: {}, product: {}", i, j, product);
+            }
+        }
+    }
+}
+
+fn part2(numbers: &[i32], size: usize) {
+    for i in 0..size {
+        for j in 0..(i+1) {
+            let partial_sum = numbers[i] + numbers[j];
+            if partial_sum >= 2020 {
+                continue;
+            }
+
+            for k in 0..(i+1) {
+                let sum = partial_sum + numbers[k];
                 if sum == 2020 {
                     let product = numbers[i] * numbers[j] * numbers[k];
                     println!("i: {}, j: {}, k: {}, product: {}", i, j, k, product);
