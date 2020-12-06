f3285f00eb4a829f3ce61680bbbc242cc779d60f
Collin Styles <collingstyles@gmail.com>
2020-12-05 22:41:32 -0800

    06: Part 1

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashSet;
+
+static INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1() {
+    let sum: usize = INPUT
+        .trim()
+        .split("\n\n")
+        .map(|group| group.lines().collect::<String>())
+        .map(|answers| count_chars(&answers))
+        .sum();
+
+    println!("part1: {}", sum);
+}
+
+fn count_chars(string: &str) -> usize {
+    let chars = string
+        .chars()
+        .fold(HashSet::new(), |mut acc, x| {
+            acc.insert(x);
+            acc
+        });
+
+    chars.len()
 }
