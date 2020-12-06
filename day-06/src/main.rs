7694fbe40fd1e492ec5cbd1ee2e3e9d658bf0968
Collin Styles <collingstyles@gmail.com>
2020-12-05 23:38:16 -0800

    06: Performance improvements
    We can speed things up by only iterating over the input once and
    calculating both parts 1 & 2 in a single pass.

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,44 +1,21 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 static INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    part1();
-    part2();
-}
-
-fn part1() {
-    let sum: usize = INPUT
+    let parts: (usize, usize) = INPUT
         .trim()
         .split("\n\n")
-        .map(|group| group.lines().collect::<String>())
-        .map(|answers| unique_chars(&answers))
-        .sum();
+        .map(|group| solve_parts(group))
+        .fold((0, 0), |(part1_sum, part2_sum), (part1, part2)| {
+            (part1_sum + part1, part2_sum + part2)
+        });
 
-    println!("part1: {}", sum);
+    println!("part1: {}", parts.0);
+    println!("part2: {}", parts.1);
 }
 
-fn unique_chars(string: &str) -> usize {
-    string
-        .chars()
-        .fold(HashSet::new(), |mut chars, x| {
-            chars.insert(x);
-            chars
-        })
-        .len()
-}
-
-fn part2() {
-    let sum: usize = INPUT
-        .trim()
-        .split("\n\n")
-        .map(|group| count_unanimities(group))
-        .sum();
-
-    println!("part2: {}", sum);
-}
-
-fn count_unanimities(group: &str) -> usize {
+fn solve_parts(group: &str) -> (usize, usize) {
     let size_of_group = group.lines().count();
 
     let answers = group
@@ -51,8 +28,12 @@ fn count_unanimities(group: &str) -> usize {
             answers
         });
 
-    answers
+    let part1 = answers.iter().count();
+
+    let part2 = answers
         .iter()
         .filter(|(_character, count)| **count == size_of_group)
-        .count()
+        .count();
+
+    (part1, part2)
 }
