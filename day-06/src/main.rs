928302c54a8958830024f8d384e60da41a5003a5
Collin Styles <collingstyles@gmail.com>
2020-12-05 23:02:21 -0800

    06: Clean up

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -12,47 +12,47 @@ fn part1() {
         .trim()
         .split("\n\n")
         .map(|group| group.lines().collect::<String>())
-        .map(|answers| count_chars(&answers))
+        .map(|answers| unique_chars(&answers))
         .sum();
 
     println!("part1: {}", sum);
 }
 
-fn count_chars(string: &str) -> usize {
-    let chars = string
+fn unique_chars(string: &str) -> usize {
+    string
         .chars()
-        .fold(HashSet::new(), |mut acc, x| {
-            acc.insert(x);
-            acc
-        });
-
-    chars.len()
+        .fold(HashSet::new(), |mut chars, x| {
+            chars.insert(x);
+            chars
+        })
+        .len()
 }
 
 fn part2() {
     let sum: usize = INPUT
         .trim()
         .split("\n\n")
-        .map(|group| group.lines().collect::<Vec<&str>>())
-        .map(|people| count_unanimities(&people))
+        .map(|group| count_unanimities(group))
         .sum();
 
     println!("part2: {}", sum);
 }
 
-fn count_unanimities(people: &[&str]) -> usize {
-    let mut answers: HashMap<char, usize> = Default::default();
-    for persons_answers in people {
-        for character in persons_answers.chars() {
+fn count_unanimities(group: &str) -> usize {
+    let size_of_group = group.lines().count();
+
+    let answers = group
+        .lines()
+        .fold(HashMap::new(), |mut answers, persons_answers| {
+            persons_answers.chars().for_each(|answer| {
+                answers.entry(answer).and_modify(|x| *x += 1).or_insert(1);
+            });
+
             answers
-                .entry(character)
-                .and_modify(|x| *x += 1)
-                .or_insert(1);
-        }
-    }
+        });
 
     answers
         .iter()
-        .filter(|(_character, count)| **count == people.len())
+        .filter(|(_character, count)| **count == size_of_group)
         .count()
 }
