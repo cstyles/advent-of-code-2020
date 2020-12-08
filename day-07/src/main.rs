b3c80c71300708213fd9ad27477d255342cae6e5
Collin Styles <collingstyles@gmail.com>
2020-12-07 21:55:29 -0800

    07: Part 2
    This is so ugly T_T

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -25,6 +25,7 @@ impl<'a> std::convert::From<(&'a str, &'a str)> for BagType<'a> {
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -70,6 +71,65 @@ fn part1() {
         to_check.append(&mut difference);
     }
 
-    println!("{:#?}", can_contain_shiny_gold);
+    // println!("{:#?}", can_contain_shiny_gold);
     println!("{:#?}", can_contain_shiny_gold.len());
+}
+
+fn part2() {
+    // Key: some bag type
+    // Value: All bag types that are directly contained by the above bag type
+    let mut hm: HashMap<&str, HashSet<(i32, &str)>> = Default::default();
+    let re = Regex::new(r#"(?P<number>\d+ )?(?P<bagtype>\w+ \w+) bags?"#).unwrap();
+
+    for line in INPUT.lines() {
+        let mut key: &str = Default::default();
+
+        for capture in re.captures_iter(line) {
+            let bagtype = capture.name("bagtype").unwrap().as_str();
+            let number = capture
+                .name("number")
+                .map(|n| n.as_str().trim().parse().unwrap());
+
+            if capture.name("number").is_none() {
+                key = bagtype;
+            } else {
+                let number = number.unwrap();
+                hm.entry(key)
+                    .and_modify(|set| {
+                        set.insert((number, bagtype));
+                    })
+                    .or_insert_with(|| hashset!((number, bagtype)));
+            }
+        }
+    }
+
+    let mut dyn_prog: HashMap<&str, i32> = Default::default();
+    let total = recursive_bags(&mut dyn_prog, &hm, "shiny gold");
+
+    println!("total: {}", total);
+}
+
+fn recursive_bags(
+    mut dyn_prog: &HashMap<&str, i32>,
+    bag_data: &HashMap<&str, HashSet<(i32, &str)>>,
+    bagtype: &str,
+) -> i32 {
+    if dyn_prog.contains_key(bagtype) {
+        *dyn_prog.get(bagtype).unwrap()
+    } else {
+        let mut total = 0;
+
+        match bag_data.get(bagtype) {
+            Some(set) => {
+                for directly_contained in set {
+                    let (number, bagtype) = directly_contained;
+                    total += number;
+                    total += number * recursive_bags(&mut dyn_prog, &bag_data, bagtype);
+                }
+            },
+            None => total = 0,
+        }
+
+        total
+    }
 }
