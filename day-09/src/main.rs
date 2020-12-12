dfe7f13f7af7acc7779e95951b6b594d7b0f5546
Collin Styles <collingstyles@gmail.com>
2020-12-11 16:23:39 -0800

    09: Part 1

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,3 +1,35 @@
+static INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let numbers: Vec<i64> = INPUT
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    let invalid_number = part1(&numbers);
+    println!("part1 = {}", invalid_number);
+}
+
+fn part1(numbers: &[i64]) -> i64 {
+    let mut start = 0;
+    let mut end = 25;
+
+    while is_valid(&numbers, start, end) {
+        start += 1;
+        end += 1;
+    }
+
+    numbers[end]
+}
+
+fn is_valid(numbers: &[i64], start: usize, end: usize) -> bool {
+    for i in start..end {
+        for j in 0..i {
+            if numbers[i] + numbers[j] == numbers[end] {
+                return true;
+            }
+        }
+    }
+
+    false
 }
