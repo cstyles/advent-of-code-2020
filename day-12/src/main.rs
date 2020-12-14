915fb2473fa64c060b1522d9f1ca825da12a8119
Collin Styles <collingstyles@gmail.com>
2020-12-13 17:23:47 -0800

    12: Part 2

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -11,10 +11,43 @@ impl Point {
     fn manhattan_distance(&self) -> i64 {
         self.x.abs() + self.y.abs()
     }
+
+    fn multiply(&self, multiplier: i64) -> Self {
+        Self {
+            x: self.x * multiplier,
+            y: self.y * multiplier,
+        }
+    }
+
+    fn add(&self, other: &Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+
+    fn rotate(&mut self, degrees: i64) -> Self {
+        match degrees {
+            90 => Point {
+                x: self.y,
+                y: -self.x,
+            },
+            180 => Point {
+                x: -self.x,
+                y: -self.y,
+            },
+            270 => Point {
+                x: -self.y,
+                y: self.x,
+            },
+            _ => panic!(),
+        }
+    }
 }
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -46,4 +79,30 @@ fn part1() {
     }
 
     println!("part1: {}", position.manhattan_distance());
+}
+
+fn part2() {
+    let mut position = Point::default();
+    let mut waypoint = Point { x: 10, y: 1 };
+
+    for instruction in INPUT.lines() {
+        let (action, value) = instruction.split_at(1);
+        let value: i64 = value.parse().unwrap();
+
+        match action {
+            "N" => waypoint.y += value,
+            "S" => waypoint.y -= value,
+            "E" => waypoint.x += value,
+            "W" => waypoint.x -= value,
+            "R" => waypoint = waypoint.rotate(value),
+            "L" => waypoint = waypoint.rotate(360 - value),
+            "F" => {
+                let vector = waypoint.multiply(value);
+                position = position.add(&vector);
+            }
+            _ => panic!(),
+        }
+    }
+
+    println!("part2: {}", position.manhattan_distance());
 }
