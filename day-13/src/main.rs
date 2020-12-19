a0313ea6e3927e2eca7d7b674d3d542caddc9190
Collin Styles <collingstyles@gmail.com>
2020-12-18 20:43:01 -0800

    13: Part 1

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,3 +1,24 @@
+static INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1() {
+    let mut lines = INPUT.lines();
+    let timestamp: usize = lines.next().unwrap().parse().unwrap();
+    let (bus_id, until): (usize, usize) = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .filter_map(|id| id.parse::<usize>().ok())
+        .map(|bus| {
+            let just_before = timestamp / bus * bus;
+            let next_bus = just_before + bus;
+            (bus, next_bus - timestamp)
+        })
+        .min_by(|&x, &y| x.1.cmp(&y.1))
+        .unwrap();
+
+    println!("part1 = {}", bus_id * until);
 }
